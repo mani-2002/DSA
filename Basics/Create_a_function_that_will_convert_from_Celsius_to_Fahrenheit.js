ff18e0e75074ef358a2d34199038a2bb4b798d3f
mani2002 <manikantavinjamuri8522@gmail.com>
2024-05-02 08:53:44 +0530

    17th code done

---
+++ Basics/Create_a_function_that_will_convert_from_Celsius_to_Fahrenheit.js
@@ -0,0 +1,5 @@
+function convertCelsiusToFahrenheit(celsius) {
+  var fahrenheit = (9 * celsius + 160) / 5;
+  console.log("entered temparature in fahrenheit :", fahrenheit);
+}
+convertCelsiusToFahrenheit(25);
