ebd3b997d3339ec4f9f33dbb9d65a16306c422b6
mani2002 <manikantavinjamuri8522@gmail.com>
2024-05-02 09:09:37 +0530

    20th code done

---
+++ Basics/20_Calculate_the_average_of_the_numbers_in_an_array_of_numbers.js
@@ -0,0 +1,6 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var sum = 0;
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+console.log("average of numbers within the given array :", sum / arr.length);
