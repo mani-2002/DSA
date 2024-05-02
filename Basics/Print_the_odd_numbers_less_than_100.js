fe54d9a2774872027f3458afa4eb6b0aea7b3969
mani2002 <manikantavinjamuri8522@gmail.com>
2024-05-02 08:29:02 +0530

    12th code done

--- Basics/Print_the_odd_numbers_less_than_100.js
+++ Basics/Print_the_odd_numbers_less_than_100.js
@@ -1,5 +1,14 @@
-for(let i = 0; i < 100; i++){
-    if(i%2!==0){
-        console.log(i);
+for(let i = 1; i <= 10; i++)
+{
+    printTable(i);
+    print("\n");
+}
+
+function printTable(n)
+{
+    for(let i = 1; i <= 10; i++)
+    {
+        let row = n + " * " + i + " = " + n * i;
+        print(row);
     }
 }
